f40fbb7baa264c244e0b84a1dea114977e6c57de
alv2017 <v.alishauskaite@gmail.com>
2025-11-11 17:57:38 +0200

    Initial commit: initial version of the application

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fastapi-Auth-Sync
+
+FastAPI application with a synchronous hashing function used for authentication.
---
+++ api/apps/auth/authenticate.py
@@ -0,0 +1,43 @@
+
+from email_validator import EmailNotValidError, validate_email
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.apps.auth.passwords import verify_password
+from api.db.schema import User as db_User
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+async def authenticate_user(
+    session: AsyncSession, username_or_email: str, password: str
+) -> db_User:
+    try:
+        validate_email(username_or_email)
+        query_filter = db_User.email
+    except EmailNotValidError:
+        query_filter = db_User.username
+
+    async with session:
+        user = (
+            (
+                await session.execute(
+                    select(db_User).where(query_filter == username_or_email)
+                )
+            )
+            .scalars()
+            .first()
+        )
+
+    if not user:
+        raise AuthenticationError(f"Invalid credentials for user: {username_or_email}")
+
+    if not verify_password(password, user.hashed_password):
+        raise AuthenticationError(f"Invalid credentials for user: {username_or_email}")
+
+    return user
+
+
+# Function to reverse an integer: example: 123 -> 321
