29d583e32272a2350b1b9b40b06c54cca6748dd7
alv2017 <v.alishauskaite@gmail.com>
2025-11-18 01:50:44 +0200

    CreateUserModel: username validation added

--- api/apps/users/models.py
+++ api/apps/users/models.py
@@ -1,5 +1,5 @@
 from fastapi.param_functions import Form
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing_extensions import Annotated, Doc
 
 
@@ -19,6 +19,13 @@ class CreateUserModel(UserBaseModel):
                 """
         ),
     ]
+
+    @field_validator("username")
+    @classmethod
+    def username_must_be_at_least_3_chars(cls, v: str) -> str:
+        if len(v) < 3:
+            raise ValueError("Username must be at least 3 characters long")
+        return v
 
 
 class ResponseUserModel(UserBaseModel):
