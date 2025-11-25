5ff97141dd705685bc231a2153b2fbeab8eb923b
alv2017 <v.alishauskaite@gmail.com>
2025-11-25 18:34:10 +0200

    Project configuration issues fixed.

--- api/config.py
+++ api/config.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from enum import Enum
 from pathlib import Path
 
@@ -10,50 +9,48 @@ from sqlalchemy import URL
 
 ROOT_DIRECTORY = Path(__file__).parent.parent
 ENV = ROOT_DIRECTORY / ".env"
-ENV_TEMPLATE = ROOT_DIRECTORY / ".env.template"
 API_LOG_FILE_LOCATION = ROOT_DIRECTORY / "logs" / "api.log"
 
 
 class RunMode(str, Enum):
-    DEV: str = "DEV"
-    PROD: str = "PROD"
-    TEST: str = "TEST"
+    DEV = "DEV"
+    PROD = "PROD"
+    TEST = "TEST"
 
 
 class LoggingMode(Enum):
-    CRITICAL: int = logging.CRITICAL
-    ERROR: int = logging.ERROR
-    WARNING: int = logging.WARNING
-    INFO: int = logging.INFO
-    DEBUG: int = logging.DEBUG
+    CRITICAL = logging.CRITICAL
+    ERROR = logging.ERROR
+    WARNING = logging.WARNING
+    INFO = logging.INFO
+    DEBUG = logging.DEBUG
 
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
-    logging: int = LoggingMode.ERROR
+    logging: int = LoggingMode.ERROR.value
     reload: bool = True
     mode: str = RunMode.PROD.value
 
 
 class AccessTokenConfig(BaseModel):
-    secret_key: str = "ChangeMe-Secret-Key-2025"
+    secret_key: str
     algorithm: str = "HS256"
     expire_minutes: int = 30
 
 
 class DBConfig(BaseModel):
-    vendor: str = "sqlite"
-    iface: str = "aiosqlite"
-    host: str = ""
-    port: str = ""
-    user: str = ""
-    name: str = os.path.join(ROOT_DIRECTORY, "sqlite", "db.sqlite")
-    password: str = ""
+    vendor: str
+    iface: str
+    host: str
+    port: str
+    user: str
+    name: str
+    password: str
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 5
-    prepared_statement_cache_size: int = 128
     max_overflow: int = 10
 
     @property
@@ -65,23 +62,21 @@ class DBConfig(BaseModel):
             host=self.host or None,
             port=int(self.port) if self.port else None,
             database=self.name or None,
-            query={
-                "prepared_statement_cache_size": str(self.prepared_statement_cache_size)
-            },
         )
 
 
 class Settings(BaseSettings):
     api_prefix: str = "/api"
-    run: RunConfig = RunConfig()
-    db: DBConfig = DBConfig()
-    access_token: AccessTokenConfig = AccessTokenConfig()
+    run: RunConfig
+    db: DBConfig
+    access_token: AccessTokenConfig
 
     model_config = SettingsConfigDict(
-        env_file=(ENV_TEMPLATE, ENV), case_sensitive=False, env_nested_delimiter="__"
+        env_file=ENV, case_sensitive=False, env_nested_delimiter="__"
     )
 
 
 settings = Settings()
+
 
 oauth2_scheme: OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl="/api/auth/token")
